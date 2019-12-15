f3d71721c820b3ab1fb2ea1366486249fae8ea16
Markus Waitl <mwaitl@posteo.de>
2019-12-15 16:13:32 +0100

    add new tick worker

---
+++ src/service-worker/tickWorker/worker.js
@@ -0,0 +1,33 @@
+let interval = null;
+let delay = 0;
+let running = false;
+let lastTick = false;
+
+const run = () => {
+  if(!running) return;
+
+  interval = setInterval(() => {
+    const currentTick = Date.now();
+
+    if (currentTick >= (lastTick + delay)) {
+      lastTick = currentTick;
+      postMessage('next_tick');
+    }
+  }, 0);
+};
+
+self.addEventListener('message', (message) => {
+  if(message.data.action === 'start') {
+    lastTick = Date.now();
+    delay = message.data.delay;
+    running = true;
+    run();
+  }
+  if(message.data.action === 'stop') {
+    running = false;
+    clearInterval(interval);
+  }
+  if(message.data.action === 'update') {
+    delay = message.data.delay;
+  }
+});
